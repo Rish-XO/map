4d43ff77cae8d1be7417e91abc9c714846d00fbb
Rish-XO <rishal.mhd009@gmail.com>
2024-03-12 20:39:18 +0530

    map

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-func main(){
+import "fmt"
+
+func main() {
 	// colors := map[string]string {
 	// 	"red":"#ff000",
 	// }
 
-	var colors map
+	colors := make(map[string]string)
+	colors["white"] = "#ffffff"
+
+	fmt.Println(colors)
 }
